04ed06001e85c60c60ce42e551636e6a7beda850
FortiBrine <info@fortibrine.cc>
2025-09-24 22:53:29 +0300

    split index.js into few files in 01 lab

--- 01_VariablesAndDataTypes/index.js
+++ 01_VariablesAndDataTypes/index.js
@@ -1,29 +1,2 @@
-function inc(n) {
-  return n + 1;
-}
 
-const a = 5;
-const b = inc(a);
-console.dir({ a, b });
 
-function inc2(obj) {
-  obj.n += 1;
-}
-
-const obj = { n: 5 };
-inc2(obj);
-console.dir(obj);
-
-const arr = [true, 'hello', 5, 12, -200, false, false, 'word', 3.14, null, undefined, {a:1}, [1,2], function(){}];
-const typeCount = {};
-
-for (const item of arr) {
-  let type = typeof item;
-  if (item === null) type = 'null';
-
-  if (!typeCount[type]) typeCount[type] = 0;
-
-  typeCount[type] += 1;
-}
-
-console.dir(typeCount);
